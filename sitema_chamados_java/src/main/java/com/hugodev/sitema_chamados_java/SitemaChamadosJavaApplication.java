70670a6503b04be6afac425b05de0e88de4f1eb6
HugoGBR <hugopn12@gmail.com>
2024-10-01 16:31:55 -0400

    ambiante spring instalado

---
+++ sitema_chamados_java/src/main/java/com/hugodev/sitema_chamados_java/SitemaChamadosJavaApplication.java
@@ -0,0 +1,13 @@
+package com.hugodev.sitema_chamados_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitemaChamadosJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitemaChamadosJavaApplication.class, args);
+	}
+
+}
---
+++ sitema_chamados_java/src/test/java/com/hugodev/sitema_chamados_java/SitemaChamadosJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugodev.sitema_chamados_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitemaChamadosJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
